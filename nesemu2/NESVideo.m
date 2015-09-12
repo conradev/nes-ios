4f6ec347baadcf68c5716f6ec6b6cb3cd0048f6e
Conrad Kramer <conrad@kramerapps.com>
2015-09-12 01:41:27 -0400

    Fix OpenGL renderer
    lol

--- nesemu2/NESVideo.m
+++ nesemu2/NESVideo.m
@@ -194,6 +194,8 @@ int video_init() {
         glBindTexture(GL_TEXTURE_2D, glScreenTexture);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glBindTexture(GL_TEXTURE_2D, 0);
         
         [EAGLContext setCurrentContext:nil];
